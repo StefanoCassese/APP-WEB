1df44dbc1568f8667d761f936fc3513c575566da
MarianoMigueles <164581983+MarianoMigueles@users.noreply.github.com>
2024-04-30 18:31:08 -0300

    Add files via upload

---
+++ six-shop-final-+verDetallesCard+FixCategoryError+FixCarritoButtom/js/main.js
@@ -0,0 +1,177 @@
+import { searchLogic } from "./searcher.js";
+import { createCategoryFilter } from "./categoryFilter.js";
+import { mainCartControler, addToCart } from "./carrito.js";
+
+function getProducts(done) {
+    const results = fetch('https://fakestoreapi.com/products');
+    results
+        .then(response => response.json())
+        .then(data => {   
+            done(data);
+        });
+}
+
+getProducts(data => {
+    writeProductHTML(data)
+});
+
+function abrirMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function cerrarMenu(event) {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickInsideIcon = icon.contains(event.target);
+    
+    if (!isClickInsideMenu && !isClickInsideIcon) {
+        menu.classList.remove("open");
+        icon.classList.remove("open");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const icon = document.querySelector(".hamburger-icon");
+    if (icon) {
+        icon.addEventListener("click", abrirMenu);
+    }
+
+    const links = document.querySelectorAll(".menu-links a");
+    links.forEach(link => {
+        link.addEventListener("click", () => {
+            const menu = document.querySelector(".menu-links");
+            const icon = document.querySelector(".hamburger-icon");
+            menu.classList.remove("open");
+            icon.classList.remove("open");
+        });
+    });
+
+    document.body.addEventListener("click", cerrarMenu);
+
+});
+
+export function writeProductHTML(data) {
+    const main = document.querySelector("main");
+
+    data.forEach(articulos => {
+        localStorage.setItem(`producto-${articulos.id}`, JSON.stringify(articulos));
+        const articulo = document.createRange().createContextualFragment(
+            `
+            <article id="${articulos.id}" class="product">
+                <div class="product__img">
+                    <img src="${articulos.image}">
+                </div>
+                <div class="product__text">
+                    <p class="product__text__title">${articulos.title}</p>
+                    <p class="product__text__price"><i class="fa-solid fa-dollar-sign"></i>${articulos.price}</p>
+                </div>
+                <div class="product__button">
+                    <button class="ver-detalles-btn btn btn-color-2 proyecto-btn" data-productId="${articulos.id}">Ver Detalles</button>
+                    <button class="add-to-cart btn btn-color-2 proyecto-btn" data-productId="${articulos.id}">Agregar al Carrito</button>
+                </div>
+            </article>
+            `
+        );
+        main.append(articulo);
+    });
+
+        // Agregar event listeners después de crear los elementos
+        const verDetallesButtons = main.querySelectorAll('.ver-detalles-btn');
+        verDetallesButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const productId = button.getAttribute('data-productId');
+                const producto = JSON.parse(localStorage.getItem(`producto-${productId}`));
+                mostrarDetallesProducto(producto);
+            });
+        });
+}
+
+
+/* resuelvo la promesa de getProducts para usarlo en una lista*/
+
+function solveGetProductsPromise() {
+    return new Promise((resolve, reject) => {
+        getProducts(data => {       
+            resolve(data);
+        });
+    });
+}
+
+async function getGlobalProductsList() {
+    let productList = await solveGetProductsPromise()
+    return productList;
+}
+
+/* acorta el text para que no supere el limite maximo asignado*/
+
+export function truncarTexto(texto, limite) {
+    if (texto.length > limite) {
+        return texto.substring(0, limite - 3) + '...';
+    } else {
+        return texto;
+    }
+}
+
+/* Logica buscador */
+
+searchLogic(await getGlobalProductsList())
+
+/* Logica categorias */
+
+createCategoryFilter(await getGlobalProductsList())
+   
+/* Logica carrito*/
+
+mainCartControler()
+
+/* Función para mostrar los detalles del producto */
+
+let tarjetaDetalleAbierta = null;
+
+export function mostrarDetallesProducto(producto) {
+    
+    if (tarjetaDetalleAbierta) {
+        document.body.removeChild(tarjetaDetalleAbierta);
+        tarjetaDetalleAbierta = null;
+    }
+    
+    const detalleProducto = document.createElement('div');
+    detalleProducto.classList.add('producto-detalle-custom');
+    
+    detalleProducto.innerHTML = `
+        <div class="detalle-producto__imagen-custom">
+            <img src="${producto.image}" alt="${producto.title}">
+        </div>
+        <div class="detalle-producto__info-custom">
+            <h2 class="detalle-producto__titulo-custom">${producto.title}</h2>
+            <p class="detalle-producto__descripcion-custom">${truncarTexto(producto.description, 500)}</p>
+            <p class="detalle-producto__precio-custom">$${producto.price}</p>
+            <div class= "product__button">
+            <button class="detalle-producto__cerrar-btn-custom btn btn-color-2 proyecto-btn">Cerrar</button>
+            <button class="add-to-cart btn btn-color-2 proyecto-btn" data-productId="${producto.id}">Agregar al Carrito</button>
+            </div>
+        </div>
+    `;
+    
+    document.body.appendChild(detalleProducto);    
+    
+    const cerrarBtn = detalleProducto.querySelector('.detalle-producto__cerrar-btn-custom');
+    cerrarBtn.addEventListener('click', () => {
+        
+        document.body.removeChild(detalleProducto);
+        tarjetaDetalleAbierta = null;
+    });
+
+    const addToCartBtn = detalleProducto.querySelector('.add-to-cart');
+    addToCartBtn.addEventListener('click', () => {
+        const productId = addToCartBtn.getAttribute('data-productId');
+        addToCart(productId);
+    });
+    
+    
+    tarjetaDetalleAbierta = detalleProducto;
+}
